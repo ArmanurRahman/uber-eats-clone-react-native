e342a281138a93b790a9596536aec8ced55bd70d
mubeen <mubeenarman19@gmail.com>
2020-10-31 14:24:14 +0900

    implementation of search grid

---
+++ conponents/SearchGridTile.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+    Platform,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    View,
+    Image,
+    Text,
+    Dimensions,
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
+const SearchGridTile = (props) => {
+    let TouchableComponent = TouchableOpacity;
+    if (Platform.OS === "android" && Platform.Version >= 21) {
+        TouchableComponent = TouchableNativeFeedback;
+    }
+
+    return (
+        <View>
+            <TouchableComponent style={{ flex: 1 }} onPress={() => {}}>
+                <View style={styles.container}>
+                    <Image
+                        source={{ uri: props.imageURL }}
+                        style={styles.image}
+                    />
+                    <Text style={styles.title}>{props.title}</Text>
+                </View>
+            </TouchableComponent>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    gridItem: {
+        margin: 15,
+        height: width / 2 - 10,
+    },
+    container: {
+        borderRadius: 10,
+        shadowColor: "black",
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        elevation: 5,
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
+    },
+    title: {
+        fontSize: 20,
+        textAlign: "right",
+        padding: 10,
+    },
+    image: {
+        width: width / 2 - 10,
+        height: width / 2 - 10,
+    },
+});
+
+export default SearchGridTile;
--- screens/food/SearchFood.jsx
+++ screens/food/SearchFood.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import slideOptionData from "../../data/slide-option-data";
+import SearchGridTile from "../../conponents/SearchGridTile";
 
 const SearchFoodScreen = (props) => {
+    const renderItem = (itemDate) => {
+        return (
+            <View style={styles.screen}>
+                <SearchGridTile
+                    title={itemDate.item.title}
+                    imageURL={itemDate.item.imageURL}
+                />
+            </View>
+        );
+    };
     return (
         <View>
-            <Text>Search page</Text>
+            <FlatList
+                data={slideOptionData}
+                keyExtractor={(item, index) => item.title}
+                renderItem={renderItem}
+                numColumns={2}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
 
 export default SearchFoodScreen;
